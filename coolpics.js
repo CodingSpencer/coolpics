e7ed1b2bfc80412d95f0a676c29897722afb2082
CodingSpencer <sashcraft121@gmail.com>
2024-07-08 12:56:14 -0600

    cool stuff

--- coolpics.js
+++ coolpics.js
@@ -1,28 +1,42 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const menu_opt = document.querySelector("header ul");
-  const menu = document.querySelector(".menu");
+const menu_opt = document.querySelector("header ul");
+const menu = document.querySelector(".menu");
+const gallery = document.querySelector(".gallery");
 
-  menu.addEventListener("click", () => {
-    menu_opt.classList.toggle("hide");
-  });
-
-  function handleResize() {
-    if (window.innerWidth > 1000) {
-      menu_opt.classList.remove("hide");
-    } else {
-      menu_opt.classList.add("hide");
-    }
-  }
-
-  function viewerTemplate(pic, alt) {
-    return `<div class="viewer">
-    <button class="close-viewer">X</button>
-    <img src="${pic}" alt="${alt}" />
-    </div>`;
-  }
-
-  function viewHandler() {}
-
-  handleResize();
-  window.addEventListener("resize", handleResize);
+menu.addEventListener("click", () => {
+  menu_opt.classList.toggle("hide");
 });
+
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menu_opt.classList.remove("hide");
+  } else {
+    menu_opt.classList.add("hide");
+  }
+}
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+  <button id="close-viewer">X</button>
+  <img src="${pic}" alt="${alt}" />
+  </div>`;
+}
+
+function viewHandler(event) {
+  const img = event.target;
+  const src = img.getAttribute("src");
+  const splitSrc = src.split("-");
+  const newImg = splitSrc[0] + "-full.jpeg";
+
+  const body = document.querySelector("body");
+  body.insertAdjacentHTML("afterbegin", viewerTemplate(newImg, "pic"));
+
+  const close = document.getElementById("close-viewer");
+  close.addEventListener("click", () => {
+    document.querySelector("viewer").remove();
+  });
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+gallery.addEventListener("click", viewHandler);
